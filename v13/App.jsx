135c14799298806559950a46baaa4a88e3c27759
lhn3464893441 <3464893441@qq.com>
2024-01-19 22:38:45 +0800

    第7天

---
+++ v13/App.jsx
@@ -0,0 +1,33 @@
+import React from "./core/React.jsx";
+// 优化更新，update只更新当前组件和子组件
+
+let focount = 1
+let bar = 'bar'
+function Foo() {
+    const [focount, setCount] = React.useState(10)
+    const [bar, setBar] = React.useState('bar')
+    function handleClick() {
+        setCount((c)=>c + 1)
+        setBar('barbar')
+    }
+    return (
+        <div >
+            fooNum：{focount}
+            <div>
+                {bar}
+            </div>
+            <button onClick={handleClick}>click</button>
+        </div>
+    );
+}
+
+let num = 1
+function App() {
+    return (
+        <div >
+            <Foo></Foo>
+        </div>
+    );
+}
+
+export default App
